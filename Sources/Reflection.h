dcd2df5bbb487e66355f16393ce6a626b40901b7
Emil Marashliev <marashliev@gmail.com>
2016-12-07 15:14:35 +0200

    Carthage integration.

---
+++ Sources/Reflection.h
@@ -0,0 +1,19 @@
+//
+//  Reflection.h
+//  Reflection
+//
+//  Created by Emil Marashliev on 12/7/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reflection.
+FOUNDATION_EXPORT double ReflectionVersionNumber;
+
+//! Project version string for Reflection.
+FOUNDATION_EXPORT const unsigned char ReflectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflection/PublicHeader.h>
+
+
